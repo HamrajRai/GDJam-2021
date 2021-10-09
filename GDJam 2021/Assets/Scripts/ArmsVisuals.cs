65309e6cc36f9a514e6b6df562a54c86e6b8f6d5
Anthony Smiderle <anth19800@hotmail.com>
2021-10-09 16:55:41 -0400

    [Add] Arms and no collision/sticking to walls

---
+++ GDJam 2021/Assets/Scripts/ArmsVisuals.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmsVisuals : MonoBehaviour
+{
+    [SerializeField] AnimationCurve lerpCurve = null;
+    [SerializeField] Transform lerpPoint = null;
+    [SerializeField] float lerpSpeed = 1.0f;
+
+    Vector3 _originalPos = Vector3.zero;
+    bool _enableLerping = true;
+    private void OnEnable()
+    {
+        _originalPos = transform.localPosition;
+        IEnumerator Lerp()
+        {
+            float x = 0.0f;
+            while (true)
+            {
+                while (x < 1.0f && _enableLerping)
+                {
+                    yield return new WaitForEndOfFrame();
+                    x += Time.deltaTime * lerpSpeed;
+                    transform.localPosition = Vector3.Lerp(_originalPos, lerpPoint.localPosition, lerpCurve.Evaluate(x));
+                }
+                x = 1.0f;
+                while (x > 0.0f && _enableLerping)
+                {
+                    yield return new WaitForEndOfFrame();
+                    x -= Time.deltaTime * lerpSpeed;
+                    transform.localPosition = Vector3.Lerp(_originalPos, lerpPoint.localPosition, lerpCurve.Evaluate(x));
+                }
+                x = 0.0f;
+                while (!_enableLerping)
+                    transform.localPosition = _originalPos;
+            }
+        }
+        StartCoroutine(Lerp());
+    }
+}
