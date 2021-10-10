576c5ef9a944f8d9514cd9eabb10e399acf3b2ea
Emory <emorywynn@gmail.com>
2021-10-10 14:15:59 -0400

    [Fixed] janky movement

--- GDJam 2021/Assets/Scripts/EnemyController.cs
+++ GDJam 2021/Assets/Scripts/EnemyController.cs
@@ -7,33 +7,55 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyController : MonoBehaviour
 {
-    Transform[] points;
+    [Tooltip("view cone in degrees which the enemy can find the player")]
+    [SerializeField] float searchFOV = 10;
+    [SerializeField] float searchDist = 5;
+
+    [Header("References")]
+    [SerializeField] Transform player;
     [SerializeField] GameObject PointHolder;
+
+    //
+    Transform[] points;
+
     // [SerializeField] float speed;
     System.Random rand = new System.Random();
-    NavMeshAgent playerAgent;
-    Vector3 nextPosition;
+    NavMeshAgent enemyAgent;
 
     private void Start()
     {
         points = PointHolder.transform.GetComponentsInChildren<Transform>();
-        nextPosition = transform.position;
-        playerAgent = GetComponent<NavMeshAgent>();
-        playerAgent.updateRotation = true;
-        playerAgent.updateUpAxis = true;
-        playerAgent.autoBraking = true;
+        enemyAgent = GetComponent<NavMeshAgent>();
+        enemyAgent.nextPosition = transform.position;
+        enemyAgent.updateRotation = true;
+        enemyAgent.updateUpAxis = true;
+        enemyAgent.autoBraking = true;
     }
 
+    bool spotted = false;
     private void Update()
     {
+        //  print("next: " + nextPosition);
+        //  print("current: " + transform.position);
 
-        if (nextPosition == transform.position)
+        if (Vector3.Distance(transform.position, enemyAgent.destination) <= .1f)
         {
-            playerAgent.CalculatePath(nextPosition = points[rand.Next(0, points.Length - 1)].position, GetComponent<NavMeshAgent>().path);
+            enemyAgent.SetDestination(points[rand.Next(1, points.Length - 1)].position);
 
+            //   playerAgent.nextPosition = (nextPosition = points[rand.Next(1, points.Length - 1)].position);
             print("move fat saa");
         }
 
+        var playerDir = (player.position - transform.position).normalized;
+        float angle = Mathf.Rad2Deg * Mathf.Acos(Vector3.Dot(transform.forward, playerDir));
+
+        print(searchFOV + " : " + angle);
+        if (Physics.Raycast(transform.position, playerDir, searchDist) || spotted)
+            if (searchFOV > angle)
+            {
+                enemyAgent.SetDestination(player.position);
+                spotted = true;
+            }
         // playerAgent.velocity = playerAgent.velocity.normalized * speed;
 
     }
